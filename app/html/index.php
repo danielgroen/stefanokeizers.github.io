fe64e02b847cbd43227b3985c91751c40afb38bb
Daniel groen <sven@studiofonkel.nl>
2017-08-29 20:03:12 +0200

    up

---
+++ app/html/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+<script type="text/javascript" src="js/main.js"></script>
+<body>
+	<h1>Daniel Groen</h1>
+	<article>
+		<header>
+			<h2>carriere</h2>
+		</header>
+	</article>
+	<article>
+	<header>
+		<h2>statistics</h2>
+	</header>
+</article>
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Stefanokeizers.nl
+* npm install
+* bower install
+* copy environment.default to environment and set the proper domainname that matches with the nginx.conf server_name on line 2
+* copy the file nginx.conf into /etc/nginx/sites-available and /etc/nginx/sites-enabled
+* run gulp build and gulp serve on the commandline
