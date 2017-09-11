fb27fa0ebb056ac162d67b407d3de2c93575be12
Daniel groen <sven@studiofonkel.nl>
2017-09-11 14:58:44 +0200

    only paralax when desktop

--- app/js/source/parallax-scroll.js
+++ app/js/source/parallax-scroll.js
@@ -1,12 +1,14 @@
 $.fn.parallax = function(strength) {
     var scroll_top = $(window).scrollTop();
-	if ( this[0].offsetTop < ( scroll_top + $(window).height() ) ) {
+	if ( this[0].offsetTop < ( scroll_top + $(window).height() )) {
 	    this.css('background-position', '0 '+ Math.round((scroll_top - this[0].offsetTop) * strength) +'px');
 	}
 };
 
 $(window).on('scroll', function() {
-    $('#header-block .inner').parallax(0.2);
-    $('#media .inner').parallax(0.2);
-    $('#contact .inner').parallax(0.2);
+	if ( $(window).width() > 768 ) {
+	    $('#header-block .inner').parallax(0.2);
+	    $('#media .inner').parallax(0.2);
+	    $('#contact .inner').parallax(0.2);
+	}
 });
