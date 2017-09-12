d71e2024d5af5da691112d9b531a7b71bbc67b79
Daniel groen <sven@studiofonkel.nl>
2017-09-12 20:55:00 +0200

    data fixed in the json

--- app/js/source/carousel.js
+++ app/js/source/carousel.js
@@ -1,55 +1,100 @@
 jQuery(function ($) {
 
-	var owl = $('.owl-carousel');
-	
-	var mobile = 360;
-	var tablet = 768;
-	var desktop = 1280;
-	var speed = 200;
+		function jsonLoader() {
+			return $.getJSON( "data/data.json", function( data ) {
+				var cards = [];
+				var performance = [];
+				var triggerOnce = true;
 
-	var itemsPerSlide = 3;
-	var itemsperslideNew = 0; 
+				// speeldata
+				$.each( data.speeldata, function( index, val ) {
+					if (val.kaarten) {
+						var tickets = '<a href="' + val.kaarten + '">Bestel kaarten</a>';
+					} else {
+						var tickets = 'uitverkocht';
+					}
 
-	owl.owlCarousel({
-		margin: 20,
-		navText: ["<svg version='1.1' id='arrow-left' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 477.175 477.175' ><path d='M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225 c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z'/></svg>","<svg version='1.1' id='arrow-right' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 477.175 477.175'><path d='M360.731,229.075l-225.1-225.1c-5.3-5.3-13.8-5.3-19.1,0s-5.3,13.8,0,19.1l215.5,215.5l-215.5,215.5 c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-4l225.1-225.1C365.931,242.875,365.931,234.275,360.731,229.075z'/></svg>"],
-		responsive : {
-		    0 : {
-				items: 1,
-				slideBy: 3,
-				nav: false,
-				mouseDrag: true,
-				stagePadding: 50,
-				center: true,
-		    },
-		    768 : {
-				items: 3,
-				mouseDrag: false,
-				nav: true,
-				slideBy: 3,
-				stagePadding: 0,
-				center: false
-		    }
-		}
-	});
+					if (Object.keys(val).length == 4) {
+						
+						if(triggerOnce == true ) {
+							triggerOnce = false;
+							var object = data.speeldata[0];
+							performance.push( "<tr><th>" + Object.keys(object)[0] + "</th><th>" + Object.keys(object)[1] + "</th><th>" + Object.keys(object)[2] + "</th><th>" + Object.keys(object)[3] + "<th></tr>" );
+						}
+					}
+					performance.push( "<tr><td>" + val.datum + "</td><td>" + val.plaats + "</td><td>" + val.theater + "</td><td>" + tickets + "</td></tr>" );
+				});
 
-	$('.media-nav a').on('click', function(e) {
-		e.preventDefault();
-		var toIndex = $(this).parent().index() * (itemsPerSlide + 1);
-		owl.trigger('to.owl.carousel', [toIndex, speed, true]);
-	});
+				$( "<table/>", {
+					"class": "data",
+					html: performance.join( "" )
+				}).appendTo( "#speeldata .inner" );
 
-	$('.media-category').each(function( index ) {
-		var htmlText = $(this).find('a').text().toLowerCase();
-		$(this).addClass(htmlText);
-	});
+				// kaartjes
+				$.each( data.mediacards, function( index, val ) {
+					cards.push( '<div class=' + val.category + '><article class="card"><h4>'+ val.title + '</h4><a href="' + val.link + '">' + val.linkText + '</a><blockquote>' + val.quote +'</blockquote></div>');
+				});
 
-	owl.on('changed.owl.carousel', function(event) {
-		var index = event.item.index + 1;
+				$( "<div/>", {
+					"class": "owl-carousel owl-theme",
+					html: cards.join( "" )
+				}).appendTo( "#media .inner" );
+				console.log('kaartjesingeladen');
+			});
+		};
 
-		var ClassOfActiveItem =  $('.owl-carousel .owl-item:nth-child(' + index + ') > ').attr('class');
-		$('.media-category.active').removeClass('active');
-		
-		$( '.media-category.' + ClassOfActiveItem ).addClass('active');
+	jsonLoader().done(function() {
+		var owl = $('.owl-carousel');
+		console.log('owl-carousel');
+		var mobile = 360;
+		var tablet = 768;
+		var desktop = 1280;
+		var speed = 200;
+
+		var itemsPerSlide = 3;
+		var itemsperslideNew = 0; 
+
+		owl.owlCarousel({
+			margin: 20,
+			navText: ["<svg version='1.1' id='arrow-left' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 477.175 477.175' ><path d='M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225 c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z'/></svg>","<svg version='1.1' id='arrow-right' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 477.175 477.175'><path d='M360.731,229.075l-225.1-225.1c-5.3-5.3-13.8-5.3-19.1,0s-5.3,13.8,0,19.1l215.5,215.5l-215.5,215.5 c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-4l225.1-225.1C365.931,242.875,365.931,234.275,360.731,229.075z'/></svg>"],
+			responsive : {
+			    0 : {
+					items: 1,
+					slideBy: 3,
+					nav: false,
+					mouseDrag: true,
+					stagePadding: 50,
+					center: true,
+			    },
+			    768 : {
+					items: 3,
+					mouseDrag: false,
+					nav: true,
+					slideBy: 3,
+					stagePadding: 0,
+					center: false
+			    }
+			}
+		});
+
+		$('.media-nav a').on('click', function(e) {
+			e.preventDefault();
+			var toIndex = $(this).parent().index() * (itemsPerSlide + 1);
+			owl.trigger('to.owl.carousel', [toIndex, speed, true]);
+		});
+
+		$('.media-category').each(function( index ) {
+			var htmlText = $(this).find('a').text().toLowerCase();
+			$(this).addClass(htmlText);
+		});
+
+		owl.on('changed.owl.carousel', function(event) {
+			var index = event.item.index + 1;
+
+			var ClassOfActiveItem =  $('.owl-carousel .owl-item:nth-child(' + index + ') > ').attr('class');
+			$('.media-category.active').removeClass('active');
+			
+			$( '.media-category.' + ClassOfActiveItem ).addClass('active');
+		})
 	})
 });
