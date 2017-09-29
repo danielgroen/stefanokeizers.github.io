477f032bfd7186eb47b1676ecf1f9b41b695072d
Daniel groen <sven@studiofonkel.nl>
2017-09-29 22:09:04 +0200

    second bg imgage

--- app/js/source/parallax-scroll.js
+++ app/js/source/parallax-scroll.js
@@ -1,16 +1,16 @@
 var breakpoint = 768;
 
 $.fn.parallax = function(strength, offset) {
-	// offset = this[0].clientHeight / offset * 100 ;
-	if (!$.isNumeric(offset)) { offset = 0;	}
-	if ( $(window).width() > breakpoint  ) {
-		if ( this[0].offsetTop < ( $(window).scrollTop() + $(window).height() )) {
- 		   this.css('background-position-y', Math.round( ( $(window).scrollTop() - this[0].offsetTop ) * strength + offset ) +'px');
+	if (this.length != 0) {
+		if (!$.isNumeric(offset)) { offset = 0;	}
+		if ( $(window).width() > breakpoint  ) {
+			if ( this[0].offsetTop < ( $(window).scrollTop() + $(window).height() )) {
+	 		   this.css('background-position-y', Math.round( ( $(window).scrollTop() - this[0].offsetTop ) * strength + offset ) +'px');
+			}
+		} else {
+		    this.css('background-position-y', '');
 		}
-	} else {
-	    this.css('background-position-y', '');
 	}
-
 };
 
 $(window).on('scroll load', function() {
