ac7200fa059b6fa499035a1e7cafcc9c045f26ea
Daniel groen <sven@studiofonkel.nl>
2017-11-09 14:31:01 +0100

    fixed iframe and cleaned up code

--- app/js/source/carousel.js
+++ app/js/source/carousel.js
@@ -6,33 +6,6 @@ jQuery(function ($) {
 				var performance = [];
 				var triggerOnce = true;
 
-				// speeldata
-				$.each( data.speeldata, function( index, val ) {
-					if (val.kaarten) {
-						var tickets = '<a class="tickets" href="' + val.kaarten + '?utm_source=' +window.location.href +'" target="_blank">Bestel kaarten</a>';
-					} else if (val.info) {
-						var tickets = '<a class="tickets" href="' + val.info + '?utm_source=' +window.location.href +'" target="_blank">Meer info</a>';
-					} else {
-						var tickets = 'uitverkocht';
-					}
-
-					if (Object.keys(val).length == 4) {
-						
-						if(triggerOnce == true ) {
-							triggerOnce = false;
-							var object = data.speeldata[0];
-							performance.push( "<tr><th>" + Object.keys(object)[0] + "</th><th>" + Object.keys(object)[1] + "</th><th>" + Object.keys(object)[2] + "</th><th>" + Object.keys(object)[3] + "<th></tr>" );
-						}
-					}
-					performance.push( "<tr><td>" + val.datum + "</td><td>" + val.plaats + "</td><td>" + val.theater + "</td><td>" + tickets + "</td></tr>" );
-				});
-
-				$( "<table/>", {
-					"class": "data",
-					html: performance.join( "" )
-				}).appendTo( "#speeldata .inner" );
-
-				// kaartjes
 				$.each( data.mediacards, function( index, val ) {
 					cards.push( '<div class=' + val.category + '><article class="card"><h4>'+ val.title + '</h4><a class="title-text" href="' + val.link  + '?utm_source=' +window.location.href +'" target="_blank">' + val.linkText + '</a><blockquote class="quotes">' + val.quote +'</blockquote></div>');
 				});
