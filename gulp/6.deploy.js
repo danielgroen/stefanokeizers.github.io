0349e99fdf275d2c1fca06b014fb72723bfa6bdb
danielgroen <@gmail.com>
2020-03-24 10:33:36 +0100

    git deploy

--- gulp/6.deploy.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp'),
-      ghPages = require('gulp-gh-pages');
-
-gulp.task('deployment', function() {
-   return gulp.src(`${global.paths.dist}/**/*`)
-              .pipe(ghPages(global.ghpagesOptions));
-});
-
-gulp.task('deploy', gulp.series('build', 'deployment'));
---
+++ todo.md
@@ -0,0 +1,5 @@
+# TODO
+* replacing favicon
+* load jquery from own library
+* fetch data instead of ajax
+* parallax over css or performance boost
